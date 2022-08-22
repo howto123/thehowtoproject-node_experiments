fbc16481813e6a69f41267e1cbf34b69ffba4145
theHowToProject <thehowtoproject123@gmail.com>
2022-08-22 11:38:16 +0200

    let the experiments begin

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql');
+require('dotenv').config();
+
+// Configure Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+
+});
+
+// Connect
+db.connect((err)=>{
+    if(!err){
+        console.log("Mysql connected");
+        console.log(".env: ", process.env.myJsVariable);
+    }
+});
+
+
+const app = express();
+
+// Create db
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if(err){console.log("error!: ", err);}
+        console.log(result);
+        res.send('database created');
+    })
+})
+
+// Create table
+app.get('createtable', () => {
+    let sql = '';
+})
+
+
+app.listen(3001, ()=>console.log("server started this is new text"));
