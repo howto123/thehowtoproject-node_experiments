ad4cf9d963bdace976d9a9bdbcfc03d2feddc5b3
theHowToProject <thehowtoproject123@gmail.com>
2022-08-23 20:49:02 +0200

    added 1 test and the example github action

---
+++ test/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('that is the description of my test suite', function() {
+    it('this test should pass no matter what', function(){
+        assert.ok(true);
+    })
+});
