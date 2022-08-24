6be1ac1c09ef1dbcffe1f9b946f3b3c547bb034a
theHowToProject <thehowtoproject123@gmail.com>
2022-08-24 10:54:44 +0200

    compy test to another location and set one to false

---
+++ app.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('this test suite is in app.test.js', function() {
+    it('this test should pass no matter what', function(){
+        assert.ok(true);
+    })
+});
