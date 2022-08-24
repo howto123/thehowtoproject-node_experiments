d14f9af982a5b7edd456c3f7e21b5a671a05d5c1
theHowToProject <thehowtoproject123@gmail.com>
2022-08-24 10:59:56 +0200

    actually set a test to false

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 
 describe('that is the description of my test suite', function() {
     it('this test should pass no matter what', function(){
-        assert.ok(true);
+        assert.ok(false);
     })
 });
