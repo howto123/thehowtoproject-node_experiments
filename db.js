e755ab7cf4685cac60b53791a5226d64b07e4522
theHowToProject <thehowtoproject123@gmail.com>
2022-08-26 21:20:47 +0200

    this time for real

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
     user: "postgres",
-    host: "localhost",
+    host: process.env.DATABASE_URL,
     port: parseInt(process.env.DB_PORT),
     database: "sayingsdb",
     password: process.env.DB_PASSWORD,
