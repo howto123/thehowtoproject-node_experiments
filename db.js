7b5566ea1e974259e2bf2211fef37aeefb894a15
theHowToProject <thehowtoproject123@gmail.com>
2022-08-26 21:48:17 +0200

    try to follow the manual

--- db.js
+++ db.js
@@ -1,8 +1,19 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    DATABASE_URL: process.env.DATABASE_URL,
-    password: process.env.DB_PASSWORD
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+/*
+const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    port: parseInt(process.env.DB_PORT),
+    database: "sayingsdb",
+    password: process.env.DB_PASSWORD,
+    
 });
-
+*/
 module.exports = client;
