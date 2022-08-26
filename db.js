394ef595736e6e63585ac87d262477530da02380
theHowToProject <thehowtoproject123@gmail.com>
2022-08-26 21:13:50 +0200

    set port via .env

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const client = new Client({
     user: "postgres",
     host: "localhost",
+    port: parseInt(process.env.DB_PORT),
     database: "sayingsdb",
     password: process.env.DB_PASSWORD,
-    port: 8085
 });
 
 module.exports = client;
