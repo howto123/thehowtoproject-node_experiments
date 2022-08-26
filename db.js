a4d559bc8f513218384327348276e7cb23ff13fc
theHowToProject <thehowtoproject123@gmail.com>
2022-08-26 22:10:12 +0200

    set up MODE in .env

--- db.js
+++ db.js
@@ -1,19 +1,24 @@
 const { Client } = require('pg');
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-/*
-const client = new Client({
-    user: "postgres",
-    host: "localhost",
-    port: parseInt(process.env.DB_PORT),
-    database: "sayingsdb",
-    password: process.env.DB_PASSWORD,
     
-});
-*/
+let config;
+if(process.env.MODE==="local"){
+    config = {
+        user: "postgres",
+        host: "localhost",
+        port: parseInt(process.env.DB_PORT),
+        database: "sayingsdb",
+        password: process.env.DB_PASSWORD,
+        
+    };
+} else {
+    // as in the heroku docs for connection with node js
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+        rejectUnauthorized: false
+        }
+    };
+}
+
+const client = new Client(config);
 module.exports = client;
